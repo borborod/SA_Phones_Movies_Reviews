e962ead95cefc55c8808d86a872115636fd34ad2
borborod <92015071+borborod@users.noreply.github.com>
2021-10-13 20:23:11 +0400

    Add files via upload

--- demo.py
+++ demo.py
@@ -1,4 +1,4 @@
-from sentiment_classifier import SentimentClassifier
+from sentiment_classifier import PhoneSentimentClassifier, MoviesSentimentClassifier
 from codecs import open
 import time
 from flask import Flask, render_template, request
@@ -11,18 +11,57 @@ model_selected = False
 model = None
 
 def get_model(inp_type):
-    if inp_type == 'clf':
+    if inp_type == 'phone_clf':
         print("Preparing classifier")
         start_time = time.time()
-        classifier = SentimentClassifier()
+        classifier = PhoneSentimentClassifier()
         print("Classifier is ready")
         print(time.time() - start_time, "seconds")
         return classifier
+    elif inp_type == 'movies_clf':
+        print("Preparing classifier")
+        start_time = time.time()
+        classifier = MoviesSentimentClassifier()
+        print("Classifier is ready")
+        print(time.time() - start_time, "seconds")
+        return classifier
+    else:
+        model_selected = False
+        model = None
 
 
 
 @app.route("/", methods=["POST", "GET"])
 def index_page(text="", model_type = '', prediction_message=""):
+    global model_selected
+    global model
+    
+    if request.method == "POST":
+        model_type = request.form["model_type"]
+        model_selected = True
+        model = get_model(model_type)
+        print('model loaded')
+        
+        
+    if model_selected:
+        print(model_type)
+        if text != '':
+            prediction_message = model.get_prediction_message(text)
+        else:
+            prediction_message = 'neutral'
+        if model_type == 'movies_clf':
+            return render_template('movies_input_review.html', text=text, model_type=model_type, prediction_message=prediction_message)
+        elif model_type == 'phone_clf':
+            return render_template('phone_input_review.html', text=text, model_type=model_type, prediction_message=prediction_message)
+        else:
+            return render_template('index.html', text=text, prediction_message=prediction_message)
+
+    
+    return render_template('index.html', text=text, prediction_message=prediction_message)
+
+
+@app.route("/phone_page", methods=["POST", "GET"])
+def phone_page(text="", model_type = '', prediction_message=""):
     global model_selected
     global model
     
@@ -38,12 +77,6 @@ def index_page(text="", model_type = '', prediction_message=""):
             logfile.write(prediction_message + '\n')
             logfile.write("</response>" + '\n')
             logfile.close()
-        else:
-            model_type = request.form["model_type"]
-            model_selected = True
-            print(model_type)
-            model = get_model(model_type)
-            print('model loaded')
         
         
     if model_selected:
@@ -51,10 +84,40 @@ def index_page(text="", model_type = '', prediction_message=""):
             prediction_message = model.get_prediction_message(text)
         else:
             prediction_message = 'neutral'
-        return render_template('input_review.html', text=text, model_type=model_type, prediction_message=prediction_message)
+        return render_template('phone_input_review.html', text=text, model_type=model_type, prediction_message=prediction_message)
+
     
     return render_template('index.html', text=text, prediction_message=prediction_message)
 
+
+@app.route("/movies_page", methods=["POST", "GET"])
+def movies_page(text="", model_type = '', prediction_message=""):
+    global model_selected
+    global model
+    
+    if request.method == "POST":
+        if model_selected: 
+            text = request.form["text"]
+            logfile = open("ydf_demo_logs.txt", "a", "utf-8")
+            print(text)
+            logfile.write("<response>" + '\n')
+            logfile.write(text + '\n')
+            prediction_message = model.get_prediction_message(text)
+            print(prediction_message)
+            logfile.write(prediction_message + '\n')
+            logfile.write("</response>" + '\n')
+            logfile.close()
+        
+        
+    if model_selected:
+        if text != '':
+            prediction_message = model.get_prediction_message(text)
+        else:
+            prediction_message = 'neutral'
+        return render_template('movies_input_review.html', text=text, model_type=model_type, prediction_message=prediction_message)
+
+    
+    return render_template('index.html', text=text, prediction_message=prediction_message)
 
 if __name__ == "__main__":
     app.run()
--- sentiment_classifier.py
+++ sentiment_classifier.py
@@ -3,9 +3,9 @@ import re
 
 
 
-class SentimentClassifier(object):
+class PhoneSentimentClassifier():
     def __init__(self):
-        self.model = joblib.load("./sentiment_clf.pkl")
+        self.model = joblib.load("./models/phone_sentiment_clf.pkl")
         self.classes_dict = {0: "Negative:(", 1: "Positive:)", -1: "prediction error"}
 
     def clean_text(self, inp_text):
@@ -26,3 +26,20 @@ class SentimentClassifier(object):
     def get_prediction_message(self, text):
         prediction = self.predict_tonality(text)
         return self.classes_dict[prediction]
+
+
+class MoviesSentimentClassifier():
+    def __init__(self):
+        self.model = joblib.load("./models/movies_sentiment_clf.pkl")
+        self.classes_dict = {0: "Negative:(", 1: "Positive:)", -1: "prediction error"}
+
+    def predict_text(self, text):
+        try:
+            return self.model.predict([text])
+        except:
+            print("prediction error")
+            return -1, 0.8
+
+    def get_prediction_message(self, text):
+        prediction = self.predict_text(text)
+        return self.classes_dict[prediction[0]]
